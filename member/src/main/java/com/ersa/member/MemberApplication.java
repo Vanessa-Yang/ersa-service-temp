e56d72cd02fe26eadfa94c57031318a26227ee9d
yangjingru <zhnv76f@dingtalk.com>
2021-02-04 18:19:50 +0800

    fixed: 去掉一些无效的代码

--- member/src/main/java/com/ersa/member/MemberApplication.java
+++ member/src/main/java/com/ersa/member/MemberApplication.java
@@ -1,14 +1,9 @@
 package com.ersa.member;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableDiscoveryClient
